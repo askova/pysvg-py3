388aec828533b5ebce2c27b0cfd9cac42fe04275
Alexey Kovalev <1566074+askova@users.noreply.github.com>
2021-01-05 20:00:46 +0300

    first draft pull request (#1)
    * Linted with autopep8
    
    * Remove whitespace in xml subElements to play nicer with Figma
    
    * Remove extra trailing space at the end of xml tags
    
    Co-authored-by: Shawn Douglas <shawn.douglas@gmail.com>

--- pysvg/parser.py
+++ pysvg/parser.py
@@ -17,61 +17,67 @@ from pysvg.structure import *
 from pysvg.style import *
 from pysvg.text import *
 
+
 def calculateMethodName(attr):
-    name=attr
-    name=name.replace(':','_')
-    name=name.replace('-','_')
-    name='set_'+name
+    name = attr
+    name = name.replace(':', '_')
+    name = name.replace('-', '_')
+    name = 'set_' + name
     return name
-    
-def setAttributes(attrs,obj):
+
+
+def setAttributes(attrs, obj):
     for attr in list(attrs.keys()):
         if hasattr(obj, calculateMethodName(attr)):
-            eval ('obj.'+calculateMethodName(attr))(attrs[attr].value)
+            eval('obj.' + calculateMethodName(attr))(attrs[attr].value)
         else:
-            print(calculateMethodName(attr)+' not found in:'+obj._elementName)
-        
+            print(
+                calculateMethodName(attr) +
+                ' not found in:' +
+                obj._elementName)
+
+
 def build(node_, object):
     attrs = node_.attributes
-    if attrs != None:
+    if attrs is not None:
         setAttributes(attrs, object)
     for child_ in node_.childNodes:
         nodeName_ = child_.nodeName.split(':')[-1]
         if child_.nodeType == Node.ELEMENT_NODE:
             try:
                 capitalLetter = nodeName_[0].upper()
-                objectinstance=eval(capitalLetter+nodeName_[1:]) ()                
-            except:
-                print('no class for: '+nodeName_)
+                objectinstance = eval(capitalLetter + nodeName_[1:])()
+            except BaseException:
+                print('no class for: ' + nodeName_)
                 continue
-            object.addElement(build(child_,objectinstance))
+            object.addElement(build(child_, objectinstance))
         elif child_.nodeType == Node.TEXT_NODE:
-            #print "TextNode:"+child_.nodeValue
-            #if child_.nodeValue.startswith('\n'):
+            # print "TextNode:"+child_.nodeValue
+            # if child_.nodeValue.startswith('\n'):
             #    print "TextNode starts with return:"+child_.nodeValue
-            #else:
-#            print "TextNode is:"+child_.nodeValue
-            #object.setTextContent(child_.nodeValue)
-            if child_.nodeValue != None and child_.nodeValue.strip() != '':
+            # else:
+            #            print "TextNode is:"+child_.nodeValue
+            # object.setTextContent(child_.nodeValue)
+            if child_.nodeValue is not None and child_.nodeValue.strip() != '':
                 # print(len(child_.nodeValue))
                 object.appendTextContent(child_.nodeValue)
-        elif child_.nodeType == Node.CDATA_SECTION_NODE:  
-            object.appendTextContent('<![CDATA['+child_.nodeValue+']]>')          
-        elif child_.nodeType == Node.COMMENT_NODE:  
-            object.appendTextContent('<!-- '+child_.nodeValue+' -->')          
+        elif child_.nodeType == Node.CDATA_SECTION_NODE:
+            object.appendTextContent('<![CDATA[' + child_.nodeValue + ']]>')
+        elif child_.nodeType == Node.COMMENT_NODE:
+            object.appendTextContent('<!-- ' + child_.nodeValue + ' -->')
         else:
-            print("Some node:"+nodeName_+" value: "+child_.nodeValue)
+            print("Some node:" + nodeName_ + " value: " + child_.nodeValue)
     return object
 
-#TODO: packageprefix ?
+# TODO: packageprefix ?
+
+
 def parse(inFileName):
     doc = minidom.parse(inFileName)
     rootNode = doc.documentElement
     rootObj = Svg()
-    build(rootNode,rootObj)
+    build(rootNode, rootObj)
     # Enable Python to collect the space used by the DOM.
     doc = None
-    #print rootObj.getXML()
+    # print rootObj.getXML()
     return rootObj
-
-
